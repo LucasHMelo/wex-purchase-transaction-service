25dbc14ecf5afac8612b1217977b244e8c288c38
uratech2025 <uratechdigital@gmail.com>
2026-04-13 22:03:47 -0300

    build: add existing projects

---
+++ tests/Wex.TransactionManagement.Application.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Wex.TransactionManagement.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/Wex.TransactionManagement.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Wex.TransactionManagement.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
