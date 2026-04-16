5c737ac1ba4e93966cff18700696660675454642
uratech2025 <uratechdigital@gmail.com>
2026-04-15 23:44:31 -0300

    tests: implementing integration tests for transactionRepository

--- tests/Wex.TransactionManagement.Infrastructure.Tests/Infra.Data.EF/Repositories/TransactionRepository/TransactionRepositoryTestFixtureCollection.cs
+++ tests/Wex.TransactionManagement.Infrastructure.Tests/Infra.Data.EF/Repositories/TransactionRepository/TransactionRepositoryTestFixtureCollection.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using Wex.TransactionManagement.IntegrationTests.Base;
+using Wex.TransactionManager.Domain.Entities;
 
 namespace Wex.TransactionManagement.IntegrationTests.Infra.Data.EF.Repositories.TransactionRepository;
 
@@ -10,4 +12,36 @@ public class TransactionRepositoryTestFixtureCollection
 public class TransactionRepositoryTestFixture
     : BaseFixture
 {
+    public decimal GetValidTransactionAmount()
+    {
+        return Faker.Random.Number(9999);
+    }
+
+    public string GetValidTransactionDescription()
+    {
+        var transactionDescription =
+            Faker.Commerce.ProductDescription();
+        if (transactionDescription.Length > 50)
+            transactionDescription =
+                transactionDescription[..50];
+        return transactionDescription;
+    }
+
+    public DateTime GetValidTransactionDate()
+    {
+        return DateTime.UtcNow;
+    }
+
+    public Transaction GetExampleTransaction()
+        => new Transaction(GetValidTransactionDescription(), GetValidTransactionAmount(), GetValidTransactionDate());
+
+    
+     public WexTransactionDbContext CreateDbContext()
+    {
+        var dbContext = new WexTransactionDbContext(
+            new DbContextOptionsBuilder<WexTransactionDbContext>()
+            .UseInMemoryDatabase("integration-tests-db")
+            .Options
+        );
+    }
 }
--- tests/Wex.TransactionManagement.Infrastructure.Tests/Infra.Data.EF/Repositories/TransactionRepository/TransactionRepositoryTests.cs
+++ tests/Wex.TransactionManagement.Infrastructure.Tests/Infra.Data.EF/Repositories/TransactionRepository/TransactionRepositoryTests.cs
@@ -1,6 +1,27 @@
 namespace Wex.TransactionManagement.IntegrationTests.Infra.Data.EF.Repositories.TransactionRepository;
 
-public class TransactionRepositoryTests
+[Collection(nameof(TransactionRepositoryTestFixture))]
+public class TransactionRepositoryTests(TransactionRepositoryTestFixture fixture)
 {
+    private readonly TransactionRepositoryTestFixture _fixture = fixture;
+
+    [Fact(DisplayName = nameof(Insert))]
+    [Trait("Integration/Infra.Data", "TransactionRepository - Repositories")]
+    public async Task Insert()
+    {
+        WexTransactionDbContext dbContext = _fixture.CreateDbContext();
+        var exampleTransaction = _fixture.GetExampleCategory();
+        var transactionRepository = new TransactionRepository(dbContext);
+
+        await transactionRepository.Insert(exampleTransaction, CancellationToken.None);
+        await dbContext.SaveChangesAsync(CancellationToken.None);
+
+        var dbTransaction = await dbContext.Categories.Find(exampleTransaction.Id);
+        dbTransaction.Should().NotBeNull();
+        dbTransaction.Name.Should().Be(exampleTransaction.Name);
+        dbTransaction.Description.Should().Be(exampleTransaction.Description);
+        dbTransaction.IsActive.Should().Be(exampleTransaction.IsActive);
+        dbTransaction.CreatedAt.Should().Be(exampleTransaction.CreatedAt);
+    }
 
 }
